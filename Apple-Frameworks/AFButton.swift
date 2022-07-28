263242b96846498697f62f3a32918ba5fde8cb1c
Guilherme Peppi <guilhermepeppi@gmail.com>
2022-07-28 12:21:01 -0300

    Building the description panel

---
+++ Apple-Frameworks/AFButton.swift
@@ -0,0 +1,27 @@
+//
+//  AFButton.swift
+//  Apple-Frameworks
+//
+//  Created by Guilherme Peppi on 28/07/22.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(Color .red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test Title")
+    }
+}
